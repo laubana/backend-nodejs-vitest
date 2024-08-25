23c3453f0a9b250a373089956738427ee3fc3391
yban <laubana@gmail.com>
2024-08-25 00:19:30 -0700

    commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const cors = require("cors");
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.static("public"));
+
+const sundaeOptions = JSON.parse(
+  fs.readFileSync("./sundae-options.json", "utf-8")
+);
+
+app.get("/scoops", (_, res) => {
+  res.json(sundaeOptions.iceCreamFlavors);
+});
+
+app.get("/toppings", (_, res) => {
+  res.json(sundaeOptions.toppings);
+});
+
+app.post("/order", (_, res) => {
+  const orderNumber = Math.floor(Math.random() * 10000000000);
+
+  res.status(201).json({ orderNumber });
+});
+
+if (require.main === module) {
+  app.listen(5000, () => console.log("Sundae server listening on port 5000!"));
+}
+
+module.exports = app;
